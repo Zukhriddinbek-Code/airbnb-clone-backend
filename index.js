095e14e68c379d8378733a0901aea91a35be2e4c
Zukhriddinbek <zuhriddinganiyev2000@gmail.com>
2024-01-20 04:32:36 +0900

    connecting to database

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+const User = require("./models/User");
 
+MONGOBD_URL =
+  "mongodb+srv://zuhriddin-tech:gGujMjlS3jhRPdmz@cluster0.wzap21l.mongodb.net/register_users?retryWrites=true&w=majority";
 const app = express();
+
+// gGujMjlS3jhRPdmz
+
+app.use(express.json());
+
+//connection between front and backend middlepoints
 app.use(
   cors({
     credentials: true,
@@ -9,8 +19,17 @@ app.use(
   })
 );
 
+mongoose.connect(MONGOBD_URL);
+
 app.get("/test", (req, res, next) => {
   res.json("test ok!");
+});
+
+//endpoint for register page sign up
+app.post("/register", (req, res, next) => {
+  const { name, email, password } = req.body;
+  User.create({ name, email, password });
+  res.json({ name, email, password });
 });
 
 app.listen(3000);
